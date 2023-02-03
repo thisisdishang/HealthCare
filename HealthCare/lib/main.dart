2fb8f0303cb5373c57e38d15727cae91ef539782
thisisdishang <dishangkumarrana@gmail.com>
2023-02-03 09:27:11 +0530

    Rename App

--- HealthCare/lib/main.dart
+++ HealthCare/lib/main.dart
@@ -8,6 +8,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
+
   SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(statusBarColor: kPrimaryLightdark));
 }
